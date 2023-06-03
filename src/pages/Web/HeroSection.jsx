d3a5e6fbe1c5bc93800b403c8767e6a1e97c6326
kirtiraj22 <kirtirajpatanwadia064@gmail.com>
2023-06-03 12:32:05 +0530

    initialized HeroSection

--- src/pages/Web/HeroSection.jsx
+++ src/pages/Web/HeroSection.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button, Img, Text } from "components";
 const HeroSection = () => {
-  return <div>HeroSection</div>;
+  return <div>HeroSection </div>;
 };
 
 export default HeroSection;
